4d3f8d9d778ad6233a7ed461874e9ae3b75d6301
yut_fut <seregey.golubev@mail.ru>
2022-06-13 16:32:08 +0300

    added using hash

--- db/db.sql
+++ db/db.sql
@@ -193,7 +193,7 @@ create unique index if not exists index_post_by_thread_path on post using hash (
 -- create unique index if not exists index_post_by_thread_created on post using btree (thread, created);
 
 drop index if exists index_post_by_thread;
-create index if not exists index_post_by_thread on post (thread);
+create index if not exists index_post_by_thread on post using hash (thread);
 
 -- `select "nickname", "about", "email", "fullname"
 --  from "user"
@@ -204,11 +204,11 @@ create index if not exists index_post_by_thread on post (thread);
 --            where forum = $1
 --        )`
 drop index if exists index_forum_user_by_forum;
-create index if not exists index_forum_user_by_forum on forum_user ("user", forum);
+create index if not exists index_forum_user_by_forum on forum_user using hash ("user", forum);
 
 -- `select "id", "title", "author", "forum", "message", "votes", "slug", "created"
 -- from "thread"
 -- where "forum" = $1`
 drop index if exists index_thread_by_forum;
-create index if not exists index_thread_by_forum on thread (forum);
+create index if not exists index_thread_by_forum on thread using hash (forum);
 
